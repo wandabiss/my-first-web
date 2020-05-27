286a7a14b0c8e6f3ed5d1fdef56298086782fdae
vvandita18 <vvandita18@gmail.com>
2020-05-27 02:38:48 -0300

    Add footer and animacion-styles with jQuery

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ My first web project already has its own responsive navigation bar
 *Challenge Web - Day II*
 
 Finish building the navigation bar. It is adaptable and functional on mobile devices. I also learned how to integrate jQuery and used a function to give functionality to the menu.
+
+*Challenge Web - Day III*
+
+I played and had fun with animations and CSS. I explored the jQuery and Javascript animation libraries. I saw tutorials of data structuring. Add a footer and use flexbox.
--- script.js
+++ script.js
@@ -12,5 +12,30 @@ jQuery('document').ready(function($){
         } else {
             menu.addClass('show');
         }
-    });   
+    });  
+});
+
+$(document).ready(function(){
+    $("header").mouseenter(function(){
+        $("header").css("background-color", "rgb(182, 150, 144  )");
+    });
+    $("header").mouseleave(function(){
+        $("header").css("background-color", "black");
+    });
+  });
+
+  $(document).ready(function(){
+    var textWrapper = document.querySelector('.ml2');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  });
 });
